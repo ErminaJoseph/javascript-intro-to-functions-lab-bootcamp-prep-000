ec52cfecb5b852722d00dcd0f8863212928f20e4
Ermina Joseph <ermina.joseph@gmail.com>
2018-02-13 20:10:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var uppercase = "HELLO"
 var lowercase = 'hello'
+<<<<<<< HEAD
 
+=======
+var mixedCase = "Hi there"
+>>>>>>> fe6b1016a8c14bf935582dec501ffa8b026ecbe0
 function shout(string){
   return string.toUpperCase()
 }
@@ -14,10 +18,17 @@ function logWhisper(string){
   console.log('hello')
 }
 function sayHiToGrandma(string){
+<<<<<<< HEAD
   if (string.toLowerCase() === string) {
   return "I can't hear you!";
   }
   if (string.toUpperCase() === string) {
+=======
+  if (string.toLowerCase() === lowercase) {
+  return "I can't hear you!";
+  }
+  if (string.toUpperCase() === uppercase) {
+>>>>>>> fe6b1016a8c14bf935582dec501ffa8b026ecbe0
   return "YES INDEED!";
   }
   if (string === "I love you, Grandma.") {
